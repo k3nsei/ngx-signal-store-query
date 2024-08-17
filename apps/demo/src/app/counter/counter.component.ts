1607bf3604c9b680bc71389cfffe80b5b763a81d
Piotr Stępniewski <190422+k3nsei@users.noreply.github.com>
2024-08-17 17:01:01 +0200

    feat(demo): update examples

--- apps/demo/src/app/counter/counter.component.ts
+++ apps/demo/src/app/counter/counter.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/c
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
+import { CounterApiService } from './counter-api.service';
 import { CounterStore } from './counter.store';
 
 @Component({
@@ -11,26 +12,17 @@ import { CounterStore } from './counter.store';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [CounterStore],
+  providers: [CounterApiService, CounterStore],
   imports: [MatButtonModule, MatIconModule],
 })
 export class CounterComponent {
   private readonly store = inject(CounterStore);
 
-  protected readonly value = this.store.count;
+  protected readonly value = computed(() => this.store.count());
 
-  public readonly isBusy = computed(() => {
-    const increase = this.store.increaseMutation.isPending();
-    const decrease = this.store.decreaseMutation.isPending();
+  protected readonly isBusy = computed(() => this.store.counterMutation.isPending());
 
-    return increase || decrease;
-  });
-
-  public increase(): void {
-    this.store.increaseMutation.mutate(1);
-  }
-
-  public decrease(): void {
-    this.store.decreaseMutation.mutate(1);
+  protected increaseBy(amount: number): void {
+    this.store.counterMutation.mutate(amount);
   }
 }
