1720b42dba7a00240ce96a4708a7f787cfeaaf5f
Piotr Stępniewski <190422+k3nsei@users.noreply.github.com>
2024-12-21 16:25:01 +0100

    feat!: migrate to ngrx signals v19
    chore(deps): bump ngrx signals query from 18.1.1 to 19.0.0
    
    chore(deps): bump tanstack angular query from 5.61.1 to 5.62.8
    
    chore(deps): bump angular from 19.0.0 to 19.0.5
    
    chore(deps): bump angular material from 19.0.0 to 19.0.4
    
    chore(deps-dev): bump angular cli from 19.0.1 to 19.0.6
    
    Release-As: 0.5.0

--- libs/ngx-signal-store-query/src/with-query.ts
+++ libs/ngx-signal-store-query/src/with-query.ts
@@ -1,13 +1,7 @@
-import {
-  type EmptyFeatureResult,
-  signalStoreFeature,
-  type SignalStoreFeature,
-  type SignalStoreFeatureResult,
-  withMethods,
-} from '@ngrx/signals';
-import { injectQuery, type QueryKey } from '@tanstack/angular-query-experimental';
+import { type SignalStoreFeatureResult, withProps } from '@ngrx/signals';
+import { type CreateQueryResult, injectQuery, type QueryKey } from '@tanstack/angular-query-experimental';
 
-import { type CreateQueryFn, type QueryMethod, type QueryProp, type QueryStore } from './types';
+import { type CreateQueryFn, type QueryProp } from './types';
 import { lowerFirst } from './utils';
 
 export const withQuery = <
@@ -17,25 +11,17 @@ export const withQuery = <
   TData = TDataFn,
   TQueryKey extends QueryKey = QueryKey,
   Input extends SignalStoreFeatureResult = SignalStoreFeatureResult,
+  Props extends object = Record<QueryProp<Name>, CreateQueryResult<TData, TError>>,
 >(
   name: Name,
-  createQueryFn: CreateQueryFn<TDataFn, TError, TData, TQueryKey, NoInfer<Input>>,
-): SignalStoreFeature<
-  Input,
-  EmptyFeatureResult & { methods: Record<QueryProp<NoInfer<Name>>, QueryMethod<NoInfer<TData>, NoInfer<TError>>> }
-> => {
-  const prop: QueryProp<NoInfer<Name>> = `${lowerFirst(name)}Query`;
+  createQueryFn: CreateQueryFn<Name, TDataFn, TError, TData, TQueryKey, Input>,
+) => {
+  const prop: QueryProp<Name> = `${lowerFirst(name)}Query`;
 
-  return signalStoreFeature(
-    withMethods((store) => {
-      const query = injectQuery(createQueryFn(store as QueryStore<NoInfer<Input>>));
+  return withProps<Input, Props>((store) => {
+    /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+    const query = injectQuery(createQueryFn(store as any));
 
-      return {
-        [prop]: new Proxy(() => query, {
-          get: (_, prop) => Reflect.get(query, prop),
-          has: (_, prop) => Reflect.has(query, prop),
-        }),
-      } as Record<QueryProp<NoInfer<Name>>, QueryMethod<NoInfer<TData>, NoInfer<TError>>>;
-    }),
-  );
+    return { [prop]: query } as Props;
+  });
 };
